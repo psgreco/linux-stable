04facb5036c51ca31a69d9dd6a460cf2ad4d2a1d
Yogesh Gaur <yogeshnarayan.gaur@nxp.com>
2018-09-29 05:33:58 +0530

    spi: add support for octal I/O data transfer
    Add flags for Octal I/O data transfer
    Required for the SPI controller which can do the data transfer (TX/RX)
    on 8 data lines e.g. NXP FlexSPI controller.
     SPI_TX_OCTAL: transmit with 8 wires
     SPI_RX_OCTAL: receive with 8 wires
    
    Signed-off-by: Yogesh Gaur <yogeshnarayan.gaur@nxp.com>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1575,6 +1575,9 @@ static int of_spi_parse_dt(struct spi_controller *ctlr, struct spi_device *spi,
 		case 4:
 			spi->mode |= SPI_TX_QUAD;
 			break;
+		case 8:
+			spi->mode |= SPI_TX_OCTAL;
+			break;
 		default:
 			dev_warn(&ctlr->dev,
 				"spi-tx-bus-width %d not supported\n",
@@ -1592,6 +1595,9 @@ static int of_spi_parse_dt(struct spi_controller *ctlr, struct spi_device *spi,
 			break;
 		case 4:
 			spi->mode |= SPI_RX_QUAD;
+			break;
+		case 8:
+			spi->mode |= SPI_RX_OCTAL;
 			break;
 		default:
 			dev_warn(&ctlr->dev,
